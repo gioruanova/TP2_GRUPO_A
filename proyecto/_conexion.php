2ec1f70ffaae441ae96337614a8463c4ae5d5e41
Jorge Ruanova <jorge.r@saffire.com>
2023-10-31 19:20:16 -0300

    eric puerto

--- proyecto/_conexion.php
+++ proyecto/_conexion.php
@@ -13,8 +13,8 @@ if ($windowsUser == "Giorgio") {
 } elseif ($windowsUser !== "Manu") {
     $conexionVariable = 'mysql:host=localhost;dbname=' . $dbName . ';charset=utf8;port=3308'; // Conexion test env en base a settings de Manu
 
-} elseif ($windowsUser !== "Eric") {
-    $conexionVariable = 'mysql:host=localhost;dbname=' . $dbName . ';charset=utf8'; // Conexion test env en base a settings de Eric
+} elseif ($windowsUser !== "DESKTOP-UD2T2NO") {
+    $conexionVariable = 'mysql:host=localhost;dbname=' . $dbName . ';charset=utf8;port=3306'; // Conexion test env en base a settings de Eric
 
 } else {
     $conexionVariable = 'mysql:host=localhost;dbname=' . $dbName . ';charset=utf8'; // Conexion test env en base a settings generica
